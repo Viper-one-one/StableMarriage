131a3b759b16682c3c8a7f205fc2e1912532b4a2
Viper-one-one <taylor.j.nastally@gmail.com>
2024-04-23 22:48:11 -0700

    more extraction methods

--- src/Project1.java
+++ src/Project1.java
@@ -1,5 +1,10 @@
 import java.nio.file.*;
-import java.util.Arrays;
+
+/*
+* Made by Taylor Nastally, 04/23/2024
+* I am aware that my method of coding is excessive and a bit weird.
+* I'll fix it later.
+* */
 
 public class Project1 {
     public static void main(String[] args) {
@@ -12,19 +17,44 @@ public class Project1 {
         char sizeChar = fileContents.charAt(0);
         int size = Character.getNumericValue(sizeChar);
         System.out.println("Size of matrices: " + sizeChar);
+
         int rangeOne = getRange(size, 1);
-        int[][] matrixOne = getMatrix(fileContents, size, rangeOne);
+        int[][] matrixOne = getPreferencesMatrix(fileContents, size, rangeOne);
         System.out.println("Extracted values from Matrix 1: ");
         printMatrix(matrixOne);
+
         int rangeTwo = getRange(size, 2);
-        int[][] matrixTwo = getMatrix(fileContents, size, rangeTwo);
+        int[][] matrixTwo = getPreferencesMatrix(fileContents, size, rangeTwo);
+        System.out.println("Extracted values from Matrix 2: ");
+        printMatrix(matrixTwo);
+
+        int [][] matchingMatrix = getMatchingMatrix(fileContents, size, rangeTwo + size + 1);
     }
 
     private static int getRange(int size, int matrixNum) {
-        return matrixNum == 1 ? 1 : size;
+        return matrixNum == 1 ? 1 : size + 1;
     }
 
-    private static int[][] getMatrix(String contents, int size, int range) {
+    private static int[][] getMatchingMatrix(String contents, int size, int range) {
+        String[] lines = contents.split("\n");
+        int[][] matrix = new int[size][size];
+
+        int row = 0;
+        for (int i = range; i < lines.length; i++) {
+            String line = lines[i].trim();
+            String[] numbers = line.split(" ");
+            for (int j = 0; j < size; j++) {
+                matrix[row][j] = Integer.parseInt(numbers[j]);
+            }
+            row++;
+            if (row > size - 1) {
+                break;
+            }
+        }
+        return matrix;
+    }
+
+    private static int[][] getPreferencesMatrix(String contents, int size, int range) {
         String[] lines = contents.split("\n");
         int[][] matrix = new int[size][size];
 
